c810881e0072eb620fae71579558a01389b2ab63
crabRang <varbk0407@gmail.com>
2024-03-18 23:48:54 +0900

    first commit

---
+++ src/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava { //HelloJava 클래스의 범위 시작
+    //파일명과 클래스 이름이 같아야 한다
+    public static void main(String[] args) { //main() 메서드의 범위 시작
+        //프로그램은 main()을 기준으로 한줄씩 실행
+        System.out.println("Hello Java");
+    } //main() 메서드의 범위 끝
+} //HelloJava 클래스의 범위 끝
